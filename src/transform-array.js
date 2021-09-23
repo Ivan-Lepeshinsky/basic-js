d811dd6bd7f571581a80761241815b8bb0bca62b
Ivan-Lepeshinsky <clo-by@mail.ru>
2021-09-23 14:33:17 +0300

    add function

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+  let newArr = [];
+  let discard = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (!discard) {
+      if (arr[i] == "--double-next" && i !== arr.length - 1) {
+        newArr.push(arr[i + 1]);
+      } else if (
+        arr[i] == "--double-prev" &&
+        i !== 0 &&
+        arr[i - 2] !== "--discard-next"
+      ) {
+        newArr.push(arr[i - 1]);
+      } else if (
+        arr[i] == "--discard-prev" &&
+        arr[i - 2] !== "--discard-next"
+      ) {
+        newArr.pop();
+      } else if (arr[i] == "--discard-next") {
+        discard = true;
+      } else if (
+        arr[i] !== "--double-prev" &&
+        arr[i] !== "--double-next" &&
+        arr[i] !== "--discard-prev"
+      ) {
+        newArr.push(arr[i]);
+      }
+    } else discard = false;
+  }
+  return newArr;
 }
--- src/what-season.js
+++ src/what-season.js
@@ -1,17 +1,27 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  if (!(date + "").includes("GMT")) throw new Error("Invalid date!");
+  if (date + "" == new Date()) throw new Error("Invalid date!");
+  if (Object.prototype.toString.call(date) !== "[object Date]")
+    throw new Error("Invalid date!");
+  let s = date.getMonth();
+  if (s >= 2 && s <= 4) return "spring";
+  else if (s >= 5 && s <= 7) return "summer";
+  else if (s >= 8 && s <= 10) return "autumn";
+  else return "winter";
 }
